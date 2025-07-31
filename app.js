b730d555103a50e467d614c22ce86c83729cf374
Vitja72 <viktor.stefan@tn.techstarter.de>
2025-07-31 13:57:09 +0200

    Speicherdatei zum Speichern neuer json

--- app.js
+++ app.js
@@ -1,46 +1,67 @@
 import http from 'http';
-import { readFile } from 'fs/promises';
-import path from 'path';
+import { readFile, writeFile } from 'fs/promises';
+import path from "path";
 import { fileURLToPath } from 'url';
 
+// find the filename with fileURLtoPath import meta, and dirname with path module dirname (where it sits) from "__filename"
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const __filename = fileURLToPath(import.meta.url); // C:\Users\vikto\git-repos\HA-Project\Node_js\nodejs_intro\app.js *bezieht sich auf den direkten path*
-const __dirname = path.dirname(__filename);  // C:\Users\vikto\git-repos\HA-Project\Node_js\nodejs_intro  *Bezieht sich auf das verzeichnis*
-const configPath = path.join(__dirname, `config.json`);
+// join the directory name with config.json for configPath
+const configPath = path.join(__dirname, "config.json");
 
-const config = JSON.parse(await readFile(configPath, 'utf8'));
-const { port, hostname } = config;
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+// use json readFileSync to open and read the file, then parse it as JSON
+const config = JSON.parse(await readFile(configPath, "utf-8"));
 
-function getRequestBody(req) {
-    return new Promise((resolve, reject) =>{
-        let body = "";
-        req.on('data', teil => {
-            body += teil.toString();
+// simulated delay
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+// deconstruct the object (order of variables doesnt matter)
+const {port, hostname} = config;
 
-        })
-        req.on("end", () => {
-            
-        })
+const postsPath = path.join(__dirname, "posts.json");
 
-    });
+// function to load posts from posts.json
+async function loadPosts() {
+    const data = await readFile(postsPath, "utf-8");
+    return JSON.parse(data)
 }
 
-let posts = [
-    {id: 1,
-    title: "Mein erster Blogbeitrag",
-    content: "Das sind die Inhalte von meinem ersten Blogbeitrag.",
-    author: "Alice",
-    date: "2025-07-29"
-    },
-    {
-        id: 2,
-        title: "Node.js Grundlagen",
-        content: "In diesem Beitrag beschribe ich die Node.js Grundlagen.",
-        author: "Bob",
-        date: "2025-07-30"
-    }
-]
+async function savePosts(posts) {
+    await writeFile(postsPath, JSON.stringify(posts, null, 2), "utf-8");
+}
+function getRequestBody(req) {
+    return new Promise((resolve,reject) => {
+        let body = "";
+        req.on("data", teil => {
+            body += teil.toString();
+        });
+        req.on("end", () => {
+            resolve(body);
+        });
+        req.on("error", fehler => {
+            reject(fehler);
+        });
+    });
+};
+
+ let posts = await loadPosts();
+ 
+ 
+ //[
+//     {id: 1,
+//     title: "Mein erster Blogbeitrag",
+//     content: "Das sind die Inhalte von meinem ersten Blogbeitrag.",
+//     author: "Alice",
+//     date: "2025-07-29"
+//     },
+//     {
+//         id: 2,
+//         title: "Node.js Grundlagen",
+//         content: "In diesem Beitrag beschribe ich die Node.js Grundlagen.",
+//         author: "Bob",
+//         date: "2025-07-30"
+//     }
+// ]
 
 let nextId = posts.length;
 
@@ -100,6 +121,7 @@ const server = http.createServer(async(req, res) => {
             newPost.date = new Date().toISOString().split("T")[0];
 
             posts.push(newPost)
+            await savePosts(posts);
             res.writeHead(201, {"Content-Type": "application/json"});
             res.end(JSON.stringify(newPost));
 
@@ -123,6 +145,7 @@ server.listen(port, hostname, () => {
     console.log(`Teste den GET /posts Endpunkt unter http://${hostname}:${port}/posts`);
     console.log(`Blogbeitrag 1 Testen: http://${hostname}:${port}/posts/1`);
     console.log(`Nicht existierende Blogbeitrag: http://${hostname}:${port}/posts/99`);
+    console.log(`Neue Beiträge werden in "posts.json" gespeichert.`)
 });
 
 
